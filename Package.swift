4c07953fd7c13726a2f4791048f1121ecb26a696
jnMars <563438383@qq.com>
2022-06-10 12:47:34 +0800

    New - divide wechat from guard

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "Guard-iOS-Wechat",
+    defaultLocalization: "en",
+    platforms: [
+        .macOS(.v10_14), .iOS(.v10), .tvOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "WeCom",
+            targets: ["WeCom"])
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .binaryTarget(
+            name: "Wechat",
+            url: "https://github.com/Authing/guard-ios-wechat/releases/download/1.0.0/Wechat.xcframework.zip",
+            checksum: "cb9c911d991c59cc11c667ae3a1e8c67fa6b74a6158d6123d1eb0658940a369d"
+        )
+    ]
+)
